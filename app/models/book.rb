09836beeed9a0272f558f518744d6c1523cb817f
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-11-07 18:10:50 +0000

    added direct Associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,4 +14,10 @@
 #  user_id          :integer
 #
 class Book < ApplicationRecord
+  belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
+
+  has_many  :group_sessions, class_name: "GroupSession", foreign_key: "book_id", dependent: :destroy
+
+  has_many  :user_books, class_name: "UserBook", foreign_key: "book_id", dependent: :destroy
+
 end
